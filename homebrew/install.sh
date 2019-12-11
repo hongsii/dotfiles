65d43f1c5da629835c2879521c05273ebf5d2312
hongsii <hongsii.dev@gmail.com>
2019-12-11 23:05:05 +0900

    Remove unnecessary command

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 if [ -x "$(command -v brew)" ]; then
     echo "Already install brew. skip this script"
     exit 0
@@ -6,15 +8,16 @@ fi
 echo "   Install homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew_file_name="BrewFile"
-if [ ! -f "$brew_file_name" ]; then
-    echo "$brew_file_name doesn't exist"
+
+SCRIPT_PATH=$(dirname $0)
+brew_file="$SCRIPT_PATH/BrewFile"
+if [ ! -f "$brew_file" ]; then
+    echo "$brew_file doesn't exist"
     exit 1
 fi
 
 brew update
-brew install mas
-brew bundle --file=./BrewFile
+brew bundle --file=$brew_file
 brew cleanup
 brew csak cleanup
 
