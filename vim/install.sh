39c23d0ef816a65d638f9cb5aa39cfd33c67aae1
hongsii <hongsii.dev@gmail.com>
2020-01-14 23:12:07 +0900

    Add install script for vim

---
+++ vim/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source ~/.vimrc
+
+# Install VimPlug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
