39c8cc39a3201382077dbed550b9ffec87e981c6
hongsii <hongsii.dev@gmail.com>
2020-01-17 01:08:54 +0900

    Update print

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 if [ -x "$(command -v brew)" ]; then
-    echo "Already install brew. skip this script"
+    echo "  > Already install brew. skip this script"
     exit 0
 fi
 
-echo "   Install homebrew"
+echo "  > Install homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
 SCRIPT_PATH=$(dirname $0)
 brew_file="$SCRIPT_PATH/BrewFile"
 if [ ! -f "$brew_file" ]; then
-    echo "$brew_file doesn't exist"
+    echo "  > $brew_file doesn't exist"
     exit 1
 fi
 
@@ -20,5 +20,3 @@ brew update
 brew bundle --file=$brew_file
 brew cleanup
 brew csak cleanup
-
-exit 0
