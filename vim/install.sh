01ab7fc30295efa4a5eace54406f0f74eea51059
hongsii <hongsii.dev@gmail.com>
2020-01-17 01:00:04 +0900

    Update script
    - Change running order
    - Modify applying vimrc

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source ~/.vimrc
-
 # Install VimPlug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Apply vimrc
+vim ~/.vimrc '+so %' +PlugInstall +qall < /dev/tty
