b5d053c4d0aadfe3136f14b63aed29caca14a4c0
hongsii <hongsii.dev@gmail.com>
2020-05-31 23:45:19 +0900

    Add file check for download

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Install VimPlug
-curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+plug_file=~/.vim/autoload/plug.vim
+if [ ! -f "$plug_file" ]; then
+    echo "Download vim-plug"
+    curl -sfLo $plug_file --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi 
 
 # Apply vimrc
 vim ~/.vimrc '+so %' +PlugInstall +qall < /dev/tty
