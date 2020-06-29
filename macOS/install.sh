535315a27a22f6f8b881e49dd20af1cebebaae4d
hongsii <hongsii.dev@gmail.com>
2020-06-29 21:26:36 +0900

    Add mission control options

--- macOS/install.sh
+++ macOS/install.sh
@@ -20,11 +20,12 @@ EOF
 ## Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-## Disable Misson Control > Automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
+## Apply Mission Control options
+defaults write com.apple.dock mru-spaces -bool false # Spaces를 최근 사용 내역에 따라 자동으로 재정렬
+defaults write com.apple.dock expose-group-apps -bool true # 응용 프로그램별로 윈도우 그룹 생성
 ## Apply dock options
 defaults write com.apple.dock autohide -bool true # 자동으로 Dock 가리기와 보기
 defaults write com.apple.dock tilesize -int 60 # 크기 (max: 128)
 defaults write com.apple.dock largesize -int 60 # 확대 (max: 128)
 defaults write com.apple.dock show-recents -bool false # Dock에서 최근 사용한 응용 프로그램 보기
-killall Dock
+killall Dock # Dock 설정 적용
