62252d37623774108abefe872bc78b1a83730c77
hongsii <hongsii.dev@gmail.com>
2020-07-31 23:07:41 +0900

    Modify a variable of dotfiles path

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,11 @@ set -e
 echo ''
 
 # Declare global variable
-DOTFILES_ROOT=$(pwd -P)
+DOTFILES_PATH=$(pwd -P)
 
 # Declare function
 include_functions() {
-  local readonly SCRIPT_DIR="$DOTFILES_ROOT/script"
+  local readonly SCRIPT_DIR="$DOTFILES_PATH/script"
   for script in $(find -H "$SCRIPT_DIR" -name "*.sh"); do
       source $script
   done
@@ -22,10 +22,10 @@ install_dotfiles() {
   info 'installing dotfiles'
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 5 -name '*.symlink' -not -path '*.git*')
+  for src in $(find -H "$DOTFILES_PATH" -maxdepth 5 -name '*.symlink' -not -path '*.git*')
   do
     dst="$HOME/.$(basename "${src%.*}")"
-    dir_path=$(dirname ${src/$DOTFILES_ROOT/.})
+    dir_path=$(dirname ${src/$DOTFILES_PATH/.})
     dir_depth=$(echo $dir_path | grep -o '/' | wc -l)
     if (( $dir_depth > 1 )); then
       dst_dir="$(echo $dir_path | sed -E 's/^\.\/[^\/]*\///g')"
