2a3fa675a93beca214a98805d1102217b04c6de3
hongsii <hongsii.dev@gmail.com>
2020-08-08 23:51:56 +0900

    Modify bitbar installation

--- bitbar/install.sh
+++ bitbar/install.sh
@@ -7,14 +7,24 @@ DOTFILES_PATH=${DOTFILES_PATH:-~/.dotfiles}
 BITBAR_PATH=$DOTFILES_PATH/bitbar
 
 source $DOTFILES_PATH/script/log.sh
-if ! [ -f $BITBAR_PATH/bitbarrc.symlink ]; then
-    info 'github contribution setup'
 
-    user ' - What is your github user name?'
-    read -e github_username
-    user ' - How many contributions do you want to see on menu bar?'
-    read -e max_contributions
-
-    sed -e "s/GITHUB_USERNAME/$github_username/g" -e "s/MAX_CONTRIBUTIONS/$max_contributions/g" $BITBAR_PATH/bitbarrc.symlink.example > $BITBAR_PATH/bitbarrc.symlink
-    success 'bitbar setup'
+if [ -f $HOME/.bitbarrc ]; then
+    while true; do
+        user "Do you want to evict?\n \
+ [y]es, [n]o"
+        read -n 1 yn
+        case $yn in
+            [Yy] ) break;;
+            [Nn] ) exit 0;;
+            * ) info "You should enter yes or no!";;
+        esac
+    done
 fi
+
+user ' - What is your github user name?'
+read -e github_username
+user ' - How many contributions do you want to see on menu bar?'
+read -e max_contributions
+
+sed -e "s/GITHUB_USERNAME/$github_username/g" -e "s/MAX_CONTRIBUTIONS/$max_contributions/g" $BITBAR_PATH/bitbarrc.example > $HOME/.bitbarrc
+success 'Setup bitbar'
