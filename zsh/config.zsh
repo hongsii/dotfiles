84cee118dd12fd091f300555a4362a86b4a9514d
hongsii <hongsii.dev@gmail.com>
2020-08-10 14:19:44 +0900

    Add history config

---
+++ zsh/config.zsh
@@ -0,0 +1,5 @@
+# History options (see http://zsh.sourceforge.net/Doc/Release/Options.html#History)
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt EXTENDED_HISTORY # save beginning timestamp and duration to history file
