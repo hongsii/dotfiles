eaf8c72faf145bb85633b7cf09e8955a1498d665
hongsii <hongsii.dev@gmail.com>
2020-10-07 22:49:47 +0900

    Modify to pass prefix for binding

--- hammerspoon/hammerspoon.symlink/modules/app-shortcut.lua
+++ hammerspoon/hammerspoon.symlink/modules/app-shortcut.lua
@@ -1,26 +1,26 @@
 -- Declare prefix key
-local prefix = {"shift", "option", "ctrl"}
+local defaultPrefix = {"shift", "option", "ctrl"}
 
 -- Declare class
 local App = {}
 local appTables = { __index = App }
-function App:new(key, name, displayName)
-    return setmetatable({ key = key, name = name, displayName = displayName or name }, appTables)
+function App:new(prefix, key, name, displayName)
+    return setmetatable({ prefix = prefix, key = key, name = name, displayName = displayName or name }, appTables)
 end
 
 local apps = {
-  App:new("c", "Google Chrome", "Chrome"),
-  App:new("i", "IntelliJ IDEA"),
-  App:new("l", "Calendar"),
-  App:new("f", "Finder"),
-  App:new("t", "iTerm 2", "iTerm2"),
-  App:new("w", "카카오워크", "카카오워크"),
-  App:new("k", "KakaoTalk", "카카오톡"),
-  App:new("m", "MySQLWorkbench"), 
-  App:new("n", "Notion"), 
-  App:new("s", "Slack"), 
-  App:new("v", "VMware Horizon Client"), 
-  App:new(",", "System Preferences", "시스템 환경설정")
+  App:new(defaultPrefix, "c", "Google Chrome", "Chrome"),
+  App:new(defaultPrefix, "i", "IntelliJ IDEA"),
+  App:new(defaultPrefix, "l", "Calendar"),
+  App:new(defaultPrefix, "f", "Finder"),
+  App:new(defaultPrefix, "t", "iTerm 2", "iTerm2"),
+  App:new(defaultPrefix, "w", "카카오워크", "카카오워크"),
+  App:new(defaultPrefix, "k", "KakaoTalk", "카카오톡"),
+  App:new(defaultPrefix, "m", "MySQLWorkbench"), 
+  App:new(defaultPrefix, "n", "Notion"), 
+  App:new(defaultPrefix, "s", "Slack"), 
+  App:new(defaultPrefix, "v", "VMware Horizon Client"), 
+  App:new(defaultPrefix, ",", "System Preferences", "시스템 환경설정")
 }
 
 local function toggleApplication(name, displayName)
@@ -34,7 +34,7 @@ end
 
 local function bindShortCut()
   for key, app in pairs(apps) do
-    hs.hotkey.bind(prefix, app.key, function() toggleApplication(app.name, app.displayName) end)
+    hs.hotkey.bind(app.prefix, app.key, function() toggleApplication(app.name, app.displayName) end)
   end
 end
 
