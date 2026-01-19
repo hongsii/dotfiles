cd577aff083dec4d73b54b893637723bcb884eac
hongsii <hongsii.dev@gmail.com>
2026-01-19 14:30:16 +0900

    Add claude alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias curltime='curl -w "@$ZSH/curl/time-format.txt"'
 alias hcopy='copied=$(history -r 1 | fzf | awk '\''{$1=""; print substr($0,2)}'\'') && echo "Copied Comand: $copied" && echo -n "$copied" | pbcopy'
+alias c='claude'
