f67aeec13b0d70a11300280946e2bc7ce991dfdd
hongsii <hongsii.dev@gmail.com>
2026-02-06 01:40:06 +0900

    Improve dot script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,12 @@ alias reload!='. ~/.zshrc'
 alias curltime='curl -w "@$ZSH/curl/time-format.txt"'
 alias hcopy='copied=$(history -r 1 | fzf | awk '\''{$1=""; print substr($0,2)}'\'') && echo "Copied Comand: $copied" && echo -n "$copied" | pbcopy'
 alias c='claude'
+
+# dot wrapper for cd support
+dot() {
+  if [ "$1" = "cd" ]; then
+    cd "$HOME/.dotfiles"
+  else
+    command dot "$@"
+  fi
+}
